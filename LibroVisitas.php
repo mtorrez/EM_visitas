77169d8b2d40fdde0ba6ad4b228a96ba533e1062
Mario <mtorrezc@gmail.com>
2016-05-18 14:10:34 -0400

    Primer commit

---
+++ LibroVisitas.php
@@ -0,0 +1,38 @@
+﻿<?php
+class LibroVisitas
+{
+    private $archivo = 'miarchivo.txt';
+    private $canal;
+    private $tamanio;
+    private $nombre;
+    private $contenido;
+
+    function __construct(){
+
+    }
+
+    public function escribirLibro($nombre, $texto){
+        $this->canal = fopen($this->archivo,"a+");
+
+        $this->nombre = "<p><strong>Nombre:</strong> ".$nombre.  "</p>";
+        $this->texto = "<p><strong>Comentario:</strong> ".$texto.  "</p> <hr>";
+		
+
+        fwrite($this->canal, $this->nombre);
+        fwrite($this->canal, $this->texto);
+
+        fclose($this->canal);
+
+    }
+
+    public function leerLibro(){
+        $this->canal = fopen($this->archivo,"a+");
+        $this->tamanio = filesize($this->archivo);
+        if ($this->tamanio > 0){
+            $this->contenido = fread($this->canal, $this->tamanio);
+        }
+        return $this->contenido;
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,47 @@
+﻿<?php
+require_once('LibroVisitas.php');
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>Libro de Visitas</title>
+</head>
+
+<body>
+
+    <h1>Libro de visitas</h1>
+    <?php
+
+    if ( isset($_POST['nombre']) && isset($_POST['texto']) ){
+        $objaux = new LibroVisitas();
+        $objaux->escribirLibro($_POST['nombre'],$_POST['texto']);
+    }
+
+    $objLibro = new LibroVisitas();
+    echo $objLibro->leerLibro();
+
+     ?>
+
+    <form action="" method="post">
+        <table>
+            <tr>
+            <td>Nombre:</td>
+            <td><input type="text" name="nombre" /></td>
+            </tr>
+            <tr>
+            <td>Comentario:</td>
+            <td><textarea name="texto"></textarea></td>
+            </tr>
+            <tr>
+            <td></td>
+            <td><input type="submit" value="Enviar" /> </td>
+            </tr>
+        </table>
+
+    </form>
+
+</body>
+
+</html>
